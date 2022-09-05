05c064b401873ef10e01c8551bf675eea95ce411
Josefinamf <jmartinezfrugoni@udesa.edu.ar>
2022-09-05 12:06:06 -0300

    componentes tarjeta, tarjetas#

--- proyecto-integrador/src/screens/Home/Home.js
+++ proyecto-integrador/src/screens/Home/Home.js
@@ -1,16 +1,19 @@
 import React, {Component} from "react";
+
+/* import Tarjetas from '../../components/tarjetas /tarjetas*/
 class Home extends Component {
     constructor(props){
         super(props)  
-        
+        this.state ={}
     }
     componentDidMount(){
         fetch('https://thingproxy.freeboard.io/fetch/https://api.deezer.com/chart/0/tracks&top?limit=10')
-        .then(response => response.json())
+        .then(response => response.json()) //parciamos a json
         .then(data => console.log("DATA",data))
         .catch(error => console.log('El error fue:'+ error))
     }
     
+    //agregar render
 
 }
 
