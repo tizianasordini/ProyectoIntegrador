796def5db0dadd1f91bdcbf7e2320a8fe1acf879
tizianasordini <tizianasordini@hotmail.com>
2022-09-05 11:47:08 -0300

    rutas

--- proyecto-integrador/src/App.js
+++ proyecto-integrador/src/App.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React from 'react'
+import {Switch, Route} from 'react-router-dom'
+import Header from './components/Header/Header'
+import Home from './screens/Home/Home'
+import VerTodas from './screens/VerTodas/VerTodas'
+import Favoritos from './screens/Favoritos/Favoritos'
+import Detalle from './screens/Detalle/Detalle'
+import NotFound from './screens/NotFound/NotFound'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
-    <div>
-      Hola
-    </div>
+    <>
+      <Header />
+      <Switch>
+        <Route path='/' exact={true} component={Home} />
+        <Route path='/VerTodas' component={VerTodas} />
+        <Route path='/Favoritos' component={Favoritos} />
+        <Route path='/Detalle/:id' component={Detalle} />
+        <Route path='' component={NotFound} />
+      </Switch>
+      <Footer />
+    </>
   );
 }
 
