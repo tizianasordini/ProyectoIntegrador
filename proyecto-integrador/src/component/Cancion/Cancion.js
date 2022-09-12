dac778f56f3a604ce434f78929dd58a88e1341ae
Josefinamf <jmartinezfrugoni@udesa.edu.ar>
2022-09-12 12:19:22 -0300

    errores en home

--- proyecto-integrador/src/component/Cancion/Cancion.js
+++ proyecto-integrador/src/component/Cancion/Cancion.js
@@ -23,15 +23,15 @@ class Cancion extends Component{
         })
     }
 
-    redner() {
+    render() {
         return (
             <React.Fragment>
                 <artincle className='cacion-tarjeta'>
-                    <Link className='cancion-contenido' to={`/detalle/id/${this.props.topCanciones.id}`}>
-                        <img src={`https://thingproxy.freeboard.io/fetch/https://api.deezer.com/chart/0/tracks&top?limit=10${this.props.topCanciones.poster_path}`} alt={this.props.topCanciones.title} />
+                    {/* <Link className='cancion-contenido' to={`/detalle/id/${this.props.topCanciones.id}`}>
+                        <img src={`https://thingproxy.freeboard.io/fetch/https://api.deezer.com/chart/0/tracks&top?limit=10${this.props.topCanciones.poster_path}`} alt={this.props.topCanciones.title} /> 
                         <h2>{this.props.topCanciones.title}</h2>
                         <p>{this.state.descripcion}</p>
-                    </Link>
+                    </Link> */} aca hay que poner otra url. 
                     {
                         this.state.descripcion === ''   //haciendo funcionalidad para ver mas o menos informacion sobre la descripcion de las tarjetas
                         ? <p onClick={() => this.masInformacion()}>Ver mas informacion</p>
