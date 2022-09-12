21c22bd3fd4daab07eabdaa771034d57344c9141
Josefinamf <jmartinezfrugoni@udesa.edu.ar>
2022-09-12 17:45:42 -0300

    arranque el buscador

--- proyecto-integrador/src/screens/Home/Home.js
+++ proyecto-integrador/src/screens/Home/Home.js
@@ -9,7 +9,8 @@ class Home extends Component {
     constructor(){  //fijarse si agregar props
         super()  
         this.state ={
-            buscador:'',
+            input:'',
+            data: [],
             topCanciones: [],
            // loader: true
 
@@ -24,8 +25,48 @@ class Home extends Component {
         }))
         .catch(error => console.log('El error fue:'+ error)) //preguntar si esta bien el catch
     }
+
+    //hacer buscador
+
+    //preenir que se recargue la pagina
+    preventRecharge(event){
+        event.preventDefault()   //acordarse que e -> es abreviacion de event!
+    }
+
+    //guardar los cambio
+
+    saveChanges(event){
+        this.setState({input: event.target.value}, () => {  //como vimos en clase target es donde se encuentra el input.(lo que queremos modificar)
+            console.log(this.state.input);
+            this.buscador()
+        })
+    }
+
+    buscador(){
+        if (this.state.input !=='') {
+            fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apikey}&query=${this.state.input}`) //aca va la api key de canciones 
+            .then (res=> res.json())
+            .then(data => {
+                this.setState ({data:data.results}, () => console.log(data.results))
+            })
+            .catch (e => console.log(e))
+        }
+    }
+
     render(){
         return (
+
+            this.state.input.length === 0 ?
+            //termino de configurar el buscador con onSubmit y OnChange
+            <React.Fragment>
+                <form onSubmit={(event) => this.preventRecharge(event)}>
+                    <input type='text' placeholder='cancion' onChange={(event)=> this.saveChanges(event)} value={this.state.input} />
+                    <input type='submit' value='submit'/>  
+                </form>
+            </React.Fragment> //falta terminar el buscador . 
+            : //si no pongo los dos puntos no separo entonces salta error. 
+
+
            /* this.state.loader === true? /*  inserto if ternario, esto nos sirve para cuando creemos el loader
             (<img src={loader} alt= 'cargando la pagina' className='imagenLoader' />) */
             <React.Fragment>
