8f75eb5707045e01769b50489eea5a6258e17400
Josefinamf <jmartinezfrugoni@udesa.edu.ar>
2022-09-17 15:22:06 -0300

    cambios en css

--- proyecto-integrador/src/component/Header/header.js
+++ proyecto-integrador/src/component/Header/header.js
@@ -20,24 +20,5 @@ function Header(props){
         </header>
       )
 }
-/*function Header(props){
-    return(
-        <>
-                <nav className="navMenu">
-                    <div className="logo">
-                        <img src="" alt=""/>
-                    </div>
-
-                    <div id="menu">
-                        <ul>
-                            {
-                                props.links.map((link, idx) => <li key={`${Date.now()}-${idx}`}>{link}</li>)
-                            }
-                        </ul>
-                    </div>
-                </nav>
-    </>
-    )
-}*/
 
 export default Header
